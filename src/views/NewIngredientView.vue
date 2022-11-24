bce031d7805d7b67a1c36d4d7e5fafd65af6dff2
Mairold <mairold.maegi@gmail.com>
2022-11-24 16:00:01 +0200

    MAIROLD-16-41 WIP

--- src/views/NewIngredientView.vue
+++ src/views/NewIngredientView.vue
@@ -6,16 +6,16 @@
     <div class="row justify-content-start mt-5">
       <div class="col-6 offset-md-2">
         <div class="form-floating ">
-          <input type="text" class="form-control" id="floatingInput" placeholder="">
+          <input v-model="ingredient.ingredientName" type="text" class="form-control" id="floatingInput" placeholder="">
           <label for="floatingInput">Toiduaine nimetus</label>
         </div>
       </div>
     </div>
     <div class="row justify-content-start mt-2 mb-4">
       <div class="col-6 offset-md-2">
-        <select class="form-select" aria-label="Default select example">
+        <select v-model="ingredient.selectedIngredientGroupId" class="form-select" aria-label="Default select example">
           <option selected>Vali grupp</option>
-          <option value="1">One</option>
+          <option v-for="ingredientGroup in " value="1">One</option>
           <option value="2">Two</option>
           <option value="3">Three</option>
         </select>
@@ -26,7 +26,7 @@
     <!--Ühikute lisamine-->
     <div class="row justify-content-start mt-5">
       <div class="col-2 offset-md-2">
-        <select v-model:un  class="form-select" aria-label="Default select example">
+        <select v-model="selectedMeasurementId" class="form-select" aria-label="Default select example">
           <option selected disabled>Vali ühik</option>
           <option v-for="unit in measurements" :value="unit.measurementId"> {{ unit.measurementName }}</option>
         </select>
@@ -38,21 +38,30 @@
 
     <!--Lubatud ühikud-->
     <div class="row justify-content-start mt-5 ">
-      <div class="table table-success table-striped offset-md-2">
-        <thead>
-        <tr>
-          <th scope="col">{#}</th>
-          <th scope="col">Ühik</th>
-          <th scope="col"></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-        </tr>
-        </tbody>
+      <div class="col-4 offset-md-2">
+        <div class="table table-success table-striped ">
+          <thead>
+          <tr>
+            <th scope="col">{#}</th>
+            <th scope="col">Ühik</th>
+            <th scope="col"></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td v-for="measurement in ingredient.allowedMeasurements">{{ measurement.allowedMeasurementName }}</td>
+            <td>
+              <button type="button" class="btn btn-sm btn-danger">Kustuta</button>
+            </td>
+          </tr>
+          </tbody>
+        </div>
+      </div>
+      <div class="col-2">
+        <div v-if="errorResponse.message.length > 0" class="alert alert-danger" role="alert">
+          {{ errorResponse.message }}
+        </div>
       </div>
     </div>
 
@@ -60,7 +69,7 @@
     <div>
       <div class="row row-cols-auto justify-content-center mt-5">
         <div class="col offset-md-2">
-          <button type="button" class="btn btn-success">Salvesta deeki</button>
+          <button v-on:click="addIngredient" type="button" class="btn btn-success">Salvesta deeki</button>
         </div>
         <div class="col pe-5 ">
           <button type="button" class="btn btn-success">Tagasi</button>
@@ -75,24 +84,58 @@ export default {
   name: "NewIngredientView",
   data: function () {
     return {
-      ingredientName: '',
+      ingredient: {
+        ingredientName: '',
+        selectedIngredientGroupId: '',
+        allowedMeasurements: [
+          {
+            allowedMeasurementId: 0,
+            allowedMeasurementName: ''
+          }
+        ],
+      },
+      ingredientGroups: [
+        {
+          ingredientGroupName: ''
+        }
+      ],
       measurements: [
         {
           measurementId: 0,
           measurementName: ''
         }
-      ]
+      ],
+      selectedMeasurementId: 0,
+      errorResponse: {
+        message: 'Proov',
+        errorCode: ''
+      }
     }
   },
 
   methods: {
 
-    addMeasurementUnit: function () {
 
+    addMeasurementUnit: function () {
+      let addedIngredient = this.measurements.find(x => x.id === this.selectedMeasurementId)
+      if (this.ingredient.allowedMeasurements.includes(addedIngredient.measurementName)) {
+        this.ingredient.allowedMeasurements.push()
+      } else {
+        this.errorResponse.message = 'Oled selle ühiku juba lisanud!'
+      }
+    },
+
+    addIngredient: function () {
+      this.$http.post("/ingredient/", this.ingredient
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
     },
 
     getAllMeasurements: function () {
-      this.$http.get("/ingredient/new")
+      this.$http.get("/ingredientsu/measurements")
           .then(response => {
             this.measurements = response.data
             console.log(response.data)
@@ -102,14 +145,21 @@ export default {
           })
     },
 
-
-  },
+    getAllIngredientGroups: function () {
+      this.$http.get("/ingredient/group")
+          .then(response => {
+            this.ingredientGroups = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  }
+  ,
   beforeMount() {
     this.getAllMeasurements()
+    this.getAllIngredientGroups()
   }
 }
 </script>
-
-<style scoped>
-
-</style>
