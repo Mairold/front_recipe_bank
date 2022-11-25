159bdd3a24ef956e71759ee9ad997bb27915f790
HelenSo <helensoodla@gmail.com>
2022-11-25 15:31:26 +0200

    Started with REC15-39 Added background image to index.html

--- src/views/AddRecipeView.vue
+++ src/views/AddRecipeView.vue
@@ -8,50 +8,49 @@
 
 
 
-      <div>
 
-      <form>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <input type="newRecipe" class="form-control" id="recipeId" placeholder="Retsepti pealkiri">
-          </div>
-        </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-
-
-            <div class="col-md-6 ">
-              <select class="form-select" aria-label="Default select example">
-                <option selected>Kategooria</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-              <select class="form-select" aria-label="Default select example">
-                <option selected>Valmistamise aeg</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-              <select class="form-select" aria-label="Default select example">
-                <option selected>Sööjate arv</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
+          <form>
+            <div class="form-row">
+              <div class="form-group col-md-12">
+                <input type="newRecipe" class="form-control" id="recipeId" placeholder="Retsepti pealkiri">
+              </div>
             </div>
-          </div>
-        </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-md-4 ">
+                <select class="form-select" aria-label="Default select example">
+                  <option selected>Kategooria</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+
+              <div class="col-md-4 ">
+                <select class="form-select" aria-label="Default select example">
+                  <option selected>Valmistamise aeg</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div class="col-md-4 ">
+                <select class="form-select" aria-label="Default select example">
+                  <option selected>Sööjate arv</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
         <div class="form-group">
         </div>
-      </form>
+        </form>
+
+
+      </div>
 
 
     </div>
-
-
-  </div>
-  </div>
   </div>
 </template>
 
