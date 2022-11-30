f6142b2d4c9389b6e71b524dbe052e16c7117349
HelenSo <helensoodla@gmail.com>
2022-11-30 05:11:19 +0200

    Started with REC15-39 Added background image to index.html

--- src/views/AddRecipeView.vue
+++ src/views/AddRecipeView.vue
@@ -13,18 +13,27 @@
             Üldandmed
             <form>
               <div class="form-row">
+
+
+                <!-- Sisesta retsepti pealkiri -->
+
                 <div class="form-group col-md-12">
                   <input type="text" class="form-control" id="recipeId" placeholder="Retsepti pealkiri">
                 </div>
               </div>
+
+              <!-- Vali retsepti kategooria-->
+
               <div class="row justify-content-center mt-3">
                 <div class="col-md-4 ">
                   <select class="form-select" aria-label="Default select example">
                     <option selected>Kategooria</option>
                     <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
                   </select>
+
+
+                  <!-- Vali retsepti valmistamise aeg-->
+
                 </div>
                 <div class="col-md-4 ">
                   <select class="form-select" aria-label="Default select example">
@@ -34,12 +43,13 @@
                     <option value="3">Three</option>
                   </select>
                 </div>
+
+                <!-- Vali retsepti sööjate arv -->
+
                 <div class="col-md-4 ">
                   <select class="form-select" aria-label="Default select example">
                     <option selected>Sööjate arv</option>
                     <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
                   </select>
                 </div>
               </div>
@@ -54,27 +64,38 @@
           <div style="border:1px solid darkslategrey; text-align: left; padding:10px; text-indent: 3px">
             Koostisosad
             <div class="row g-3">
+
+              <!-- Vali retsepti koostisosa -->
+
               <div class="col-md-3">
                 <select class="form-select" aria-label="Default select example">
                   <option selected>Koostisosa, ühik</option>
                   <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
                 </select>
               </div>
+
+              <!-- Vali retsepti koostisosa ühiku kogus -->
+
               <div class="col-md-2">
                 <input type="newRecipe" class="form-control" id="recipeId" placeholder="500">
               </div>
+
+              <!-- Vali retsepti koostisosa ühiku  -->
+
+
               <div class="col-md-2">
                 <select class="form-select" aria-label="Default select example">
                   <option selected>Ühik</option>
                   <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
                 </select></div>
+
+              <!-- NUPP: lisa rida retsepti  -->
+
               <div class="col-md-1">
                 <input type="image" src="plus.ico.png" height="30" width="30"/>
               </div>
+
+              <!-- Vali retsepti valmistamise juhend -->
 
               <div class="col-md-4">
                 <div class="form-floating">
@@ -90,12 +111,19 @@
     </div>
     <div class="form-group col-md-12">
 
+      <!-- NUPP: Lisa deeki uus toiduaine-->
+
       <div class="row-cols-md-6">
         <button type="button" class="btn btn-success">Lisa deeki uus toiduaine</button>
       </div>
 
+      <!-- NUPP: Salvesta retsept retseptide kataloogi -->
+
       <button type="button" class="btn btn-success">Salvesta retsept</button>
     </div>
+
+
+    <!-- NUPP: Salvesta retsept retseptide kataloogi ja lisa õige menüü külge -->
 
     <div class="form-group col-md-12">
       <div class="row-cols-md-3">
@@ -105,8 +133,7 @@
           <select class="form-select" aria-label="Default select example">
             <option selected>Vali grupp</option>
             <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+
           </select>
         </div>
       </div>
