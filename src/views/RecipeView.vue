35bf83fd7c42b38cc83afd84bd87f9f379d0b1dd
Kristiina <kristiina.saarniit@yahoo.com>
2022-12-06 17:45:56 +0200

    Vaata retsepti vaade WIP

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -5,7 +5,7 @@
       <div class="row justify-content-between m-2">
         <div class="col-10">
           <div class="input-group">
-            <h2>Retsepti nimetus</h2>
+            <h3>{{ recipe.recipeName }}</h3>
             <!-- Retsepti nimi genereeritakse dünaamiliselt otse andmebaasist ning muutmine on disabled-->
           </div>
         </div>
@@ -20,80 +20,71 @@
       </div>
     </div>
 
+    <div class="row justify-content-start m-5">
+      <div class="col-4">
+        <div class="input-group mb-3">
+          <span class="input-group-text">Kategooria</span>
+          <input disabled v-model="recipe.categoryName" type="text" class="form-control">
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="input-group mb-3">
+          <span class="input-group-text">Ajakulu</span>
+          <input disabled v-model="recipe.prepTime" type="text" class="form-control">
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="input-group mb-3">
+          <span class="input-group-text">Sööjate arv</span>
+          <input  v-model="recipe.servingSize" class="form-control" type="number" id="servingSizeInput">
+<!--          -->
+        </div>
+      </div>
 
-    <div class="row justify-content-center m-5">
-<h3>Siia tulevad ühele reale kõrvuti tulpades kategooria, ajakulu ja inimeste arv.</h3>
-<!--      Kategooria ja ajakulu on fikseeritud ja disabled. Tulevad andmebaasist.-->
-<!--      ServingSize tuleb andmebaasist default nagu on salvestatud.-->
-<!--      Aga võiks olla muudetav ning koostisosade kogused võiksid muutuda vastavalt servingSize muutmisele-->
+      <!--      Kategooria ja ajakulu on fikseeritud ja disabled. Tulevad andmebaasist.-->
+      <!--      ServingSize tuleb andmebaasist default nagu on salvestatud.-->
+      <!--      Aga võiks olla muudetav ning koostisosade kogused võiksid muutuda vastavalt servingSize muutmisele-->
 
     </div>
 
     <div class="container">
       <div class="row justify-content-between m-2">
-<!--        <div class="col col-lg-6">-->
-        <div class="col-6">
-          <div class="row">
+        <!--        <div class="col col-lg-6">-->
+        <div class="col-5">
+
           <div>
-<!--            <h4>Siia tulevad koostisosad koos koguste ja ühikutega:</h4>-->
+            <h4>Koostisosad</h4>
             <!--   Kasutan rohelist tabelit ja tabeli päisesse kiri Koostisosad.
             Koostisosade kogust peaks saama muuta vastavalt servingSize'ile -->
 
             <table class="table table-striped table-hover">
               <thead>
               <tr>
-               <th scope="col">Koostisosad</th>
+                <th scope="col">Toiduaine</th>
                 <th scope="col">kogus</th>
                 <th scope="col">ühik</th>
               </tr>
               </thead>
               <tbody>
-              <tr>
-                <td>Sibul</td>
-                <td>2</td>
-                <td>tk</td>
-              </tr>
-              <tr>
-                <td>Spagetid</td>
-                <td>300</td>
-                <td>g</td>
-              </tr>
-              <tr>
-                <td>Purustatud tomatid, 400g</td>
-                <td>1</td>
-                <td>purk</td>
+              <tr v-for="ingredient in ingredients" :key="ingredient.ingredientId">
+                <td>{{ ingredient.ingredientName }}</td>
+                <td>{{ ingredient.quantity / initialServingSize * recipe.servingSize }}</td>
+                <td>{{ ingredient.measureUnitName }}</td>
               </tr>
               </tbody>
             </table>
-          </div>
-          </div>
-<!--          <div class="col col-lg-2">-->
-          <div class="col-12">
-            <div class="row">
-          <div>
-            <h4>Siia tulevad valmistamise juhised pealkirjaga "Valmistamine":</h4>
-            <!--   See on fikseeritud ja muutmatu. Nii nagu andmebaasist tuleb.-->
-            <table class="table table-striped table-hover">
-              <thead>
-              <tr>
-                <th scope="col">Valmistamine</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td>Pane kõrvitsatükid potti, kalla peale nii palju puljongit (või vesi + puljongipulber), et kõrvitsatükid oleksid kaetud. Lisa puljongipulber. Kuumuta keemiseni.
-                  Haki sibul, prae õlis kuldseks. Lisa koos praadimisõliga kõrvitsatükkidele. Keeda kõrvitsatükke koos praetud sibulaga, kuni kõrvitsatükid on pehmed (aeg oleneb kõrvitsatüübist).
-                  Kui kõrvits on pehme, siis püreesta kannmikseris või köögikombainis või saumiksriga ühtlaseks. Lisa sulatatud juust ning püreesta uuesti. Maitsesta vajadusel soola ja pipraga.
-                  Lase supil 10-15 minutit taheneda, siis jaota kaussidesse ja serveeri. Peale nirista paksemat kõrvitsaseemneõli.
-                  NIPP: Kui soovid, võid kaunistada supi ka röstitud kõrvitsaseemnetega</td>
-              </tr>
-              </tbody>
-            </table>
-
-          </div>
-          </div>
           </div>
         </div>
+        <div class="col-7">
+          <div>
+            <h4>Valmistamise juhised</h4>
+            <!--   See on fikseeritud ja muutmatu. Nii nagu andmebaasist tuleb.-->
+                        <div class="m-5" >
+                          <p class="textBackground" > {{ recipe.instructions }}</p>
+                        </div>
+          </div>
+        </div>
+
       </div>
     </div>
 
@@ -107,12 +98,62 @@ export default {
   name: "RecipeView",
 
   data: function () {
-    return {}
+    return {
+      recipeId: Number(sessionStorage.getItem('recipeId') ),
+
+      recipe: {
+        categoryName: '',
+        prepTime: '',
+        recipeName: '',
+        servingSize: 0,
+        instructions: ''
+      },
+      ingredients: [
+        {
+          ingredientName: '',
+          quantity: 0,
+          measureUnitName: ''
+        }
+      ],
+
+      initialServingSize: 0,
+    }
   },
   methods: {
     closeRecipe: function () {
 
-    }
-  }
+    },
+
+    getRecipe: function () {
+      this.$http.get("/recipe/content", {
+            params: {
+              recipeId: this.recipeId
+            }
+          }
+      ).then(response => {
+        this.recipe = response.data
+        this.initialServingSize = response.data.servingSize
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    getRecipeIngredients: function () {
+      this.$http.get("/ingredient/in-recipe", {
+            params: {
+              recipeId: this.recipeId
+            }
+          }
+      ).then(response => {
+        this.ingredients = response.data
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+  },
+  beforeMount() {
+    this.getRecipe()
+    this.getRecipeIngredients()
+  },
 }
 </script>
